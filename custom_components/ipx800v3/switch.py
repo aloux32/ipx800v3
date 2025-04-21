90484bb2d683c7376c571cb80d919e9bac905eee
Rémi ALBAN <59766320+remialban@users.noreply.github.com>
2025-04-21 11:38:33 +0200

    Resolve a problem linked to extension

--- custom_components/ipx800v3/switch.py
+++ custom_components/ipx800v3/switch.py
@@ -6,12 +6,17 @@ from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.device_registry import DeviceInfo, format_mac
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from homeassistant.helpers.update_coordinator import CoordinatorEntity, DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 from .const import DOMAIN
 from .board import Api
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+):
     from .board import IPX800v3
 
     board: IPX800v3 = hass.data[DOMAIN][entry.entry_id]
@@ -19,12 +24,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 
 
 class Relay(CoordinatorEntity, SwitchEntity):
-    def __init__(self, coordinator: DataUpdateCoordinator, pin: int, device: DeviceInfo, api: Api):
+    def __init__(
+        self, coordinator: DataUpdateCoordinator, pin: int, device: DeviceInfo, api: Api
+    ):
         super().__init__(coordinator, str(pin))
         self._pin = pin
         self._attr_device_info = device
         self._api = api
-        self._attr_unique_id = format_mac(device.get("serial_number")) + "-" + str(self._pin) + "-switch"
+        self._attr_unique_id = (
+            format_mac(device.get("serial_number")) + "-" + str(self._pin) + "-switch"
+        )
         self._attr_name = "Switch " + str(pin)
 
     @callback
@@ -38,15 +47,15 @@ class Relay(CoordinatorEntity, SwitchEntity):
             pass
 
     async def async_turn_on(self, **kwargs: Any):
-        await self._api.call_api("preset.htm?led" + str(self._pin) + "=1")
+        await self._api.call_api("preset.htm?set" + str(self._pin) + "=1")
         await self.coordinator.async_refresh()
 
     async def async_turn_off(self, **kwargs: Any):
-        await self._api.call_api("preset.htm?led" + str(self._pin) + "=0")
+        await self._api.call_api("preset.htm?set" + str(self._pin) + "=0")
         await self.coordinator.async_refresh()
 
     async def async_toggle(self, **kwargs: Any):
-        await self._api.call_api("leds.cgi?led=" + str(self._pin-1))
+        await self._api.call_api("leds.cgi?led=" + str(self._pin - 1))
         await self.coordinator.async_refresh()
 
     def set_state(self, state: bool):
